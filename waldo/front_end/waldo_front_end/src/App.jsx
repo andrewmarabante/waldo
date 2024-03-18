b29c024010f6868a2151a571d42319b29e8cbb05
andrewmarabante <andrewmarabante@gmail.com>
2024-03-17 19:27:14 -0700

    Created a new vite project and gutted

---
+++ waldo/front_end/waldo_front_end/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>Working</div>
+  )
+}
+
+export default App
