ffd2f1c79abb5f6a4b03af63078c03a8e1d40b26
andrewmarabante <andrewmarabante@gmail.com>
2024-03-17 21:05:27 -0700

    Created event listener that creates a circle around pointer

--- waldo/front_end/waldo_front_end/src/Game.jsx
+++ waldo/front_end/waldo_front_end/src/Game.jsx
@@ -1,5 +1,34 @@
+import './styles/Game.css'
+
 export default function Game(){
+
+    const viewCircle = (e) => {
+        const leftOffSet = e.target.offsetLeft;
+        const topOffSet = e.target.offsetTop;
+        const x = e.clientX - leftOffSet;
+        const y = e.clientY - topOffSet;
+
+        console.log('x: '+x)
+        console.log('y: '+y)
+
+        circle.style.width = '0px';
+        circle.style.height = '0px';
+        circle.style.left = x + 'px';
+        circle.style.top = y + 'px';
+      
+        setTimeout(() => {
+          circle.style.width = '70px';
+          circle.style.height = '70px';
+          circle.style.left = (x +leftOffSet - 35) + 'px';
+          circle.style.top = (y +topOffSet - 35) + 'px';
+        }, 10);
+    }
+
     return(
-        <div>Game</div>
+        <div>
+            <h1>Where's Waldo</h1>
+            <img id='waldoSnowPic' src="public/waldo_snow.jpeg" alt="waldo_pic" width={'100%'} height={"auto"} onClick={viewCircle}/>
+            <div id='circle'></div>
+        </div>
     )
 }
