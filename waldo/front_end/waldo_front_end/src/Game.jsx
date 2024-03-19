363aeedd286e9af803bf7f874c0bfb7cc3a22114
andrewmarabante <andrewmarabante@gmail.com>
2024-03-18 19:25:05 -0700

    Created a popup if game is over

--- waldo/front_end/waldo_front_end/src/Game.jsx
+++ waldo/front_end/waldo_front_end/src/Game.jsx
@@ -2,21 +2,28 @@ import './styles/Game.css'
 import MenuListComposition from './components/MenuList';
 import { useState } from 'react';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
+import Button from '@mui/material/Button';
 
 export default function Game(){
     const [circlePosition, setCirclePosition] = useState({ x: null, y: null });
     const [leftRatio, setLeftRatio] = useState(null);
     const [topRatio, setTopRatio] = useState(null);
     const [menuOpen, setMenuOpen] = useState(false);
-    const [waldo, setWaldo] = useState(null);
-    const [wenda, setWenda] = useState(null);
-    const [odlaw, setOdlaw] = useState(null);
-    const [wizard, setWizard] = useState(null);
+    const [waldo, setWaldo] = useState(false);
+    const [wenda, setWenda] = useState(false);
+    const [odlaw, setOdlaw] = useState(false);
+    const [wizard, setWizard] = useState(false);
+    const [incorrect, setIncorrect] = useState(false);
+    const [correct, setCorrect] = useState(false);
+    const [chooseMessage, setChooseMessage] = useState(false);
 
     const viewCircle = (e) => {
 
         const leftOffSet = e.target.offsetLeft;
         const topOffSet = e.target.offsetTop;
+        setIncorrect(false);
+        setCorrect(false)
+        setChooseMessage(true)
 
         if(
             menuOpen
@@ -24,6 +31,7 @@ export default function Game(){
             &&e.clientY + window.scrollY - topOffSet === circlePosition.y
             ){
             setMenuOpen(false);
+            setChooseMessage(false);
         }else{
             setMenuOpen(true);
         }
@@ -50,6 +58,7 @@ export default function Game(){
             &&e.clientY + window.scrollY - topOffSet === circlePosition.y
             )   {
             circle.style.display = 'none'
+            setChooseMessage(false)
           }else{circle.style.display = 'block'}
 
         circle.style.left = (xpos +leftOffSet - 25) + 'px';
@@ -72,42 +81,87 @@ export default function Game(){
             credentials: 'include'
         })
         .then(result => result.json())
-        .then(answer => console.log(answer))
+        .then(answer => {
+            if(answer === 'correct')
+            {
+                setCorrect(true)
+                setChooseMessage(false)
+                switch(name){
+                    case 'waldo':
+                        setWaldo(true);
+                        break;
+                    case 'wenda':
+                        setWenda(true);
+                        break;
+                    case 'odlaw':
+                        setOdlaw(true);
+                        break;
+                    case 'wizard':
+                        setWizard(true);
+                        break;
+                }
+            }else{
+                setIncorrect(true)
+                setChooseMessage(false)
+            }
+        })
         .catch(err => console.log(err))
     }
 
     const closeMenu = (name) => {
-        console.log(name)
         isCorrect(name)
         setMenuOpen(false);
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
     return(
         <div>
-            <h1>Where's Waldo</h1>
+            {(waldo && wenda && odlaw && wizard) && 
+            <div className='game'>
+                <h1>You Win!</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username" style={{fontSize:'.7em'}}>Enter Your Username:</label>
+                    <br></br>
+                    <input type="text" name='username' style={{minHeight:'2em', minWidth:'20em', textAlign:'center'}}/>
+                    <br></br>
+                    <Button type='submit' id='subButton'>Submit</Button>
+                    </form>
+
+            </div>}
+
+
+            <h1 style={{textAlign:'center'}}>Where's Waldo?</h1>
             <img id='waldoSnowPic' src="public/waldo_snow.jpeg" alt="waldo_pic" width={'100%'} height={"auto"} onClick={viewCircle}/>
             <div id='circle'></div>
             <MenuListComposition circle={circlePosition} open={menuOpen} onClose={closeMenu}></MenuListComposition>
+            {correct && <div className='message'>Found Him!</div>}
+            {incorrect && <div className='message'>That's not him!</div>}
+            {chooseMessage && <div className='message'>Choose a Character!</div>}
+            {(!correct && !incorrect && !chooseMessage) && <div className='message'>Find All The Characters Below!</div>}
             <div style={{display:'flex', justifyContent:'space-around', alignItems:'center'}}>
                 
                 <div className='characterContainer'>
                     <img src="/public/Waldo.png" alt="waldo" height={'100px'} width={'auto'}/>
-                    {waldo && <DoneOutlineIcon></DoneOutlineIcon>}
+                    {waldo && <DoneOutlineIcon style={{position:'absolute'}}></DoneOutlineIcon>}
                 </div>
 
                 <div className='characterContainer'>
                     <img src="/public/Wenda.png" alt="Wenda" height={'100px'} width={'auto'}/>
-                    {wenda &&<DoneOutlineIcon></DoneOutlineIcon>}
+                    {wenda &&<DoneOutlineIcon style={{position:'absolute'}}></DoneOutlineIcon>}
                 </div>
               
                 <div className='characterContainer'>
                     <img src="/public/Odlaw.png" alt="Odlaw" height={'100px'} width={'auto'}/>
-                    {odlaw && <DoneOutlineIcon></DoneOutlineIcon>}
+                    {odlaw && <DoneOutlineIcon style={{position:'absolute'}}></DoneOutlineIcon>}
                 </div>
 
                 <div className='characterContainer'>
                     <img src="/public/Wizard.png" alt="Wizard" height={'100px'} width={'auto'}/>
-                    {wizard && <DoneOutlineIcon></DoneOutlineIcon>}
+                    {wizard && <DoneOutlineIcon style={{position:'absolute'}}></DoneOutlineIcon>}
                 </div>
             </div>
         </div>
